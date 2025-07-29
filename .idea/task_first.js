cadf7b538f82bbc3ea47ce7a27706fc7bc8cca86
tolya199720 <tolya199720@gmail.com>
2025-07-29 22:48:11 +0300

    work_first

---
+++ .idea/task_first.js
@@ -0,0 +1,38 @@
+let a = `hello`
+console.log(a)
+let s =`owu`
+console.log(s)
+let d = `com`
+console.log(d)
+let f =`ua`
+console.log(f)
+let g =1
+console.log(g)
+let h =10
+console.log(h)
+let j =-999
+console.log(j)
+let q =123
+console.log(q)
+let w =3.14
+console.log(w)
+let e =2.7
+console.log(e)
+let r =16
+console.log(r)
+let t =true
+console.log(t)
+let y = false
+console.log(y)
+
+let firstName = `анатолій`
+let middleName = `петухов`;
+let lastName = `сергійович`;
+let person = firstName + middleName + lastName
+console.log(person)
+let aa = 100;
+console.log(typeof(aa))
+let bb =`100`;
+console.log(typeof(bb))
+let cc = true;
+console.log(typeof(cc))
